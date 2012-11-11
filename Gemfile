cbffb436fe65188506094b8b7de88980f0fb14da
Jin Zhou <ferryzhou@gmail.com>
2012-11-10 20:43:58 -0500

    data_mapper with sqlite3 memory works now

--- Gemfile
+++ Gemfile
@@ -3,3 +3,8 @@ gem 'sinatra'
 
 gem 'hpricot'
 
+gem 'data_mapper'
+
+gem "sqlite3", :groups => [:development, :test]
+
+gem 'dm-sqlite-adapter', :groups => [:development, :test]
--- hello.rb
+++ hello.rb
@@ -2,11 +2,48 @@ require 'rubygems'
 require 'sinatra'
 require './mp3link'
 
+require 'data_mapper'
+
+#################
+# Configuration #
+#################
+
+class Mp3link
+    include DataMapper::Resource
+    property :id, Serial
+    property :name, String
+    property :link, Text
+    property :created_at, DateTime
+end
+
+#configure :development do
+#  DataMapper.setup(:default, 'sqlite3://mp3links.sqlite3')
+#end
+#configure :test do
+  DataMapper.setup(:default, 'sqlite::memory:')
+#end
+DataMapper.finalize
+
+Mp3link.auto_upgrade!
+
 get '/hello' do
   "Hello World!"
 end
 
 get '/q' do
+  obj = Mp3link.first(:name => params[:name])
+  p "from database: #{obj}"
+  if obj.nil?
+    p 'not found in database'
+    link = query_mp3(utf8_to_gb2312(params[:name]))
+    Mp3link.create(:name => params[:name], :link => link, :created_at => Time.now )
+    link
+  else
+    obj.link
+  end
+end
+
+get '/rq' do
   query_mp3(utf8_to_gb2312(params[:name]))
 end
 
